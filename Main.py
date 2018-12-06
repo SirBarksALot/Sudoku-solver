d616d330ec800e3a7370b3daa001d7481c99a1f8
Ulose <44412197+Ulose@users.noreply.github.com>
2018-12-06 12:16:19 +0100

    adding new functions

--- Main.py
+++ Main.py
@@ -1,20 +1,22 @@
 column = 9
 row = 9
+num_of_tries = 0
+
 
 # grid init
 # puzzle_map = [[0 for i in range(column)] for j in range(row)]
 
 
 # setting map
-puzzle_map = [[0, 0, 0, 2, 6, 0, 7, 0, 1],
-              [6, 8, 0, 0, 7, 0, 0, 9, 0],
-              [1, 9, 0, 0, 0, 4, 5, 0, 0],
-              [8, 2, 0, 1, 0, 0, 0, 4, 0],
-              [0, 0, 4, 6, 0, 2, 9, 0, 0],
-              [0, 5, 0, 0, 0, 3, 0, 2, 8],
-              [0, 0, 9, 3, 0, 0, 0, 7, 4],
-              [0, 4, 0, 0, 5, 0, 0, 3, 6],
-              [7, 0, 3, 0, 1, 8, 0, 0, 0]]
+puzzle_map = [[5, 3, 0, 0, 7, 0, 0, 0, 0],
+              [6, 0, 0, 1, 9, 5, 0, 0, 0],
+              [0, 9, 8, 0, 0, 0, 0, 6, 0],
+              [8, 0, 0, 0, 6, 0, 0, 0, 3],
+              [4, 0, 0, 8, 0, 3, 0, 0, 1],
+              [7, 0, 0, 0, 2, 0, 0, 0, 6],
+              [0, 6, 0, 0, 0, 0, 2, 8, 0],
+              [0, 0, 0, 4, 1, 9, 0, 0, 5],
+              [0, 0, 0, 0, 8, 0, 0, 7, 9]]
 
 
 # printing map, y = number of rows
@@ -33,7 +35,7 @@ def creating_puzzle_map(grid, x, y):
 
 # finds next blank cell to fill
 # x = number of columns, y = number of rows
-def finding_blank_cell(grid, x, y):
+def find_blank_cell(grid, x, y):
     for i in range(y):
         for j in range(x):
             if grid[i][j] == 0:
@@ -41,7 +43,7 @@ def finding_blank_cell(grid, x, y):
                 return i, j
             else:
                 pass
-    return False
+    return -1, -1
 
 
 # checks whether chosen number = num is valid choice within row, column, sector
@@ -62,4 +64,32 @@ def validate_number(grid, i, j, num, x, y):
     return False
 
 
-print(validate_number(puzzle_map, 0, 5, 5, column, row))
+# solving sudoku by:
+# searching for the first blank cell
+# if all cells full then return True
+# trying numbers from 1,10 in found blank cell
+# and running it against validator
+def solver(grid, i=0, j=0):
+    global num_of_tries, column, row
+
+    # search for the next blank cell
+    i, j = find_blank_cell(grid, column, row)
+    if i == -1:
+        return True
+
+    for n in range(1, 10):
+        if validate_number(grid, i, j, n, column, row) is True:
+            grid[i][j] = n
+            if solver(grid, i, j) is True:
+                return True
+
+            # reset cell in case of fail
+            num_of_tries += 1
+            grid[i][j] = 0
+
+    return False
+
+
+solver(puzzle_map)
+printing(puzzle_map, row)
+print(num_of_tries)
