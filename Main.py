a15a7439156ffe84ece7cd3851f53ffaa0d22e7c
Ulose <44412197+Ulose@users.noreply.github.com>
2018-12-06 11:10:14 +0100

    adding new functions

--- Main.py
+++ Main.py
@@ -1,21 +1,8 @@
 column = 9
 row = 9
 
-puzzle_map = [[0 for i in range(column)] for j in range(row)]
-
-
-# printing map
-def printing(grid, y):
-    for i in range(y):
-        print(grid[i])
-
-
-# manually enter map
-def creating_puzzle_map(grid, x, y):
-    for i in range(y):
-        for j in range(x):
-            grid[i][j] = input('grid[{}][{}]: '.format(j+1, i+1))
-    return grid
+# grid init
+# puzzle_map = [[0 for i in range(column)] for j in range(row)]
 
 
 # setting map
@@ -30,9 +17,22 @@ puzzle_map = [[0, 0, 0, 2, 6, 0, 7, 0, 1],
               [7, 0, 3, 0, 1, 8, 0, 0, 0]]
 
 
-printing(puzzle_map, row)
+# printing map, y = number of rows
+def printing(grid, y):
+    for i in range(y):
+        print(grid[i])
 
 
+# manually enter map (not recommended :D)
+# x = number of columns, y = number of rows
+def creating_puzzle_map(grid, x, y):
+    for i in range(y):
+        for j in range(x):
+            grid[i][j] = input('grid[{}][{}]: '.format(j+1, i+1))
+
+
+# finds next blank cell to fill
+# x = number of columns, y = number of rows
 def finding_blank_cell(grid, x, y):
     for i in range(y):
         for j in range(x):
@@ -44,4 +44,22 @@ def finding_blank_cell(grid, x, y):
     return False
 
 
-print(finding_blank_cell(puzzle_map, column, row))
+# checks whether chosen number = num is valid choice within row, column, sector
+# i = num row, j = num column
+# x = number of columns, y = number of rows
+def validate_number(grid, i, j, num, x, y):
+    valid_row = all(num != grid[i][var] for var in range(0, x))
+    if valid_row is True:
+        valid_column = all(num != grid[var][j] for var in range(0, y))
+        if valid_column is True:
+            vari = 3 * (i//3)
+            varj = 3 * (j//3)
+            for n in range(vari, vari+3):
+                for m in range(varj, varj+3):
+                    if grid[n][m] == num:
+                        return False
+            return True
+    return False
+
+
+print(validate_number(puzzle_map, 0, 5, 5, column, row))
